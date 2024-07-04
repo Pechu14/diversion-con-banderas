19481a17b96e3408531f29fe7a9abe62f8c6ea6f
Pechu14 <jesus.gil.santos1992@gmail.com>
2024-07-04 11:52:11 +0200

    terminado

--- js/script.js
+++ js/script.js
@@ -1,40 +1,17 @@
-
-
-/*async function fetchBanderas() {
-    try {
-        // Paso 1: Hacer la solicitud fetch
-        const response = await fetch('https://restcountries.com/v3/all');
-        
-        // Verificar si la respuesta es correcta
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-    }
-    .then(data => {
-            // Paso 3: Manipular el DOM para mostrar el array
-            const countriesList = document.createElement('ul');
-                // Crear una lista para mostrar los datos
-                data.forEach(item => {
-                    const li = document.createElement('li');
-                    li.textContent = JSON.stringify(item); // Convertir cada ítem a string
-                    ul.appendChild(li);
-    })
-    })
-    .catch(error => {
-        console.error('Hubo un problema en el fetch:', error);
-    });
-}*/
-
 function mostrarCuadrado(bandera) {
     const conDetalledDiv = document.getElementById('conDetalles')
     conDetalledDiv.style.display = 'block';
     
-    conDetalledDiv.innerHTML = '<img src= ' + bandera.flags[1] + '></img>' +
-                                '<h2>' + bandera.name.common + '</h2>' +
-                                '<p>Capital: ' + bandera.capital + '</p>' +
-                                '<p>Población:' + bandera.population + '</p>' +
-                                '<p>Lado de la carretera: ' + bandera.car.side +'</p>'
+    conDetalledDiv.innerHTML = '<div class="conDetalles">' +
+                                    '<div class="bandera">' +
+                                        '<img src= ' + bandera.flags[1] + '></img></div>' +
+
+                                        '<div class="detalles"><h2>' + bandera.name.common + '</h2>' +
+                                            '<p>Capital: ' + bandera.capital + '</p>' +
+                                            '<p>Población:' + bandera.population + '</p>' +
+                                            '<p>Lado de la carretera: ' + bandera.car.side +'</p>' +
+                                        '</div>' + 
+                                '</div>'
 
     const buttonCerrar = document.createElement('button');
     buttonCerrar.innerHTML = 'Cerrar'
@@ -47,25 +24,27 @@ function ocultarCuadrado() {
     conDetalledDiv.style.display = 'none';
 }
 
+const mostartEnPantalla = (banderas) => {
+    const countriesList = document.getElementById('countries-list');
+    banderas.forEach(bandera => {
+        const divBandera = document.createElement('div');
+        divBandera.className = 'sinDetalles';
+        divBandera.innerHTML = '<img src=' + bandera.flags[1] + '> </img>' +
+                                    '<p>' + bandera.name.common + '</p>'
+        divBandera.addEventListener('click', () => mostrarCuadrado(bandera));
+        countriesList.appendChild(divBandera);
+    })
+} 
 
-function fetchBanderasConThen() {
+let banderas = ''
+
+
+function fetchBanderas() {
     fetch('https://restcountries.com/v3/all')
         .then(response => response.json())
         .then((response) => {
             let banderas = response;
-            const mostartEnPantalla = (banderas) => {
-                const countriesList = document.getElementById('countries-list');
-            
-                banderas.forEach(bandera => {
-                    const divBandera = document.createElement('div');
-                    divBandera.className = 'sinDetalles';
-                    divBandera.innerHTML = '<img src=' + bandera.flags[1] + '> </img>' +
-                                                '<p>' + bandera.name.common + '</p>'
-                    divBandera.addEventListener('click', () => mostrarCuadrado(bandera));
-                    countriesList.appendChild(divBandera);
-                })
-                
-            }
+            banderas.sort((a, b) => a.name.common.localeCompare(b.name.common));
             mostartEnPantalla(banderas)
         })
         .catch((error) => {
@@ -73,24 +52,4 @@ function fetchBanderasConThen() {
         });
 }
 
-
-
-
-
-fetchBanderasConThen();
-
-
-
-
-
-
-
-
-
-
-/*async function fetchBanderasConAwait() {
-    const response = await fetch('https://restcountries.com/v3/all');
-    const banderas = await response.json()
-    return banderas
-}*/
-// const banderasPromesa = fetchBanderasConAwait()
+fetchBanderas();
